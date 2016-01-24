f8fa403508ad8ba1fbd6acd17f55e43ae97110cd
Aravinda Rao <maniacalace@gmail.com>
2016-01-25 04:35:42 +0530

    Implement first cut of player server

---
+++ src/player/server.py
@@ -0,0 +1,64 @@
+import random
+
+from flask import Flask, jsonify
+import requests
+
+
+app = Flask(__name__)
+logger = app.logger
+
+DEFENSE_MATRIX_SIZE = 5
+
+
+@app.route('/choose-number/')
+def choose_number():
+    number = random.randrange(1, 11)  # Abstract away choosing of number
+    return jsonify(**{
+        'chosen_number': number
+    })
+
+
+@app.route('/make-defense-matrix/')
+def make_defense_matrix():
+    matrix = [
+        random.randrange(1, 11) for _ in range(0, DEFENSE_MATRIX_SIZE)
+    ]
+    return jsonify(**{
+        'defense_matrix': matrix
+    })
+
+
+def connect_to_referee():
+    # TODO: Add retrying logic
+    referee_host = 'localhost'
+    referee_port = 5000
+    url = 'http://{host}:{port}/tournament/connect/'.format(
+        host=referee_host, port=referee_port
+    )
+    player_id = 1
+    requests.post(url, data={
+        'player_id': player_id
+    })
+
+    # TODO: This logger doesn't work since 'app' isn't runnning yet.
+    logger.info('Player ID: {} is connected to server!'.format(player_id))
+    logger.info('what')
+
+
+if __name__ == '__main__':
+    # TODO: Do this asynchronously so server is allowed to start? grequests?
+    connect_to_referee()
+    app.run(debug=True, port=5001)
+
+
+'''
+TODO
+
+- Fix pre-app.run() logging.
+- Read defense matrix length from some config
+- Take port as param (or randomize)
+
+Refactoring:
+    - Move BL out of routes and into service file
+
+'''
