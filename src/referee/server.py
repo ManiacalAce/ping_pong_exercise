5551f18eabfeb32207614b8e80a688d82c1bf627
Aravinda Rao <maniacalace@gmail.com>
2016-01-25 06:06:23 +0530

    Handle 're-connect' scenario

--- src/referee/server.py
+++ src/referee/server.py
@@ -1,6 +1,6 @@
 import threading
 
-from flask import Flask, request, jsonify
+from flask import (Flask, request, jsonify, abort)
 
 from player import Player
 from tournament import (MAX_TOURNEY_PLAYERS, connected_players)
@@ -20,10 +20,13 @@ def connect():
     player_id = int(request.form['player_id'])
     player_host = request.form['player_host']
     player_port = int(request.form['player_pot'])
-    player = Player(player_id, player_host, player_port)
-    connected_players[player_id] = player
 
-    # TODO: Disallow reconnects
+    # Players shouldn't be able to 'reset' data by re-connecting
+    if player_id in connected_players:
+        return abort(400)
+    else:
+        player = Player(player_id, player_host, player_port)
+        connected_players[player_id] = player
 
     logger.info('Player ID: {} connected!'.format(player_id))
 
