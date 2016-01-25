f3c3525dfbc44e7dcef4182ff775a48d9bac0335
Aravinda Rao <maniacalace@gmail.com>
2016-01-25 08:56:33 +0530

    Add run.py placeholder

---
+++ run.py
@@ -0,0 +1,10 @@
+
+def main():
+    print(
+        'This script will run the demo. But it hasn\'t been implemented yet!\n'
+        'Coming soon!\n'
+    )
+
+
+if __name__ == '__main__':
+    main()
